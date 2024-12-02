22399a7b12d3271d72af86a425f0107f0da3796f
acse976 <antonio.pereira@sdggroup.com>
2024-12-02 20:46:05 +0100

    Update streamlit_app.py
    requests added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
 st.write(
@@ -38,6 +39,10 @@ if ingredients_list:
     
     #st.write(my_insert_stmt)
     #st.stop()
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
     time_to_insert = st.button("Submit order")
 
     if time_to_insert:
